293cd94671da4c4f47e6c5bfdff342a702929967
Shamanovi4 <roma.chernitskyi@gmail.com>
2023-03-04 06:48:02 +0200

    Projects and skills update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import {FaGithub} from 'react-icons/fa'
-import {Section} from '../../hoc/Section/Section'
+import { FaGithub } from 'react-icons/fa'
+import { Section } from '../../hoc/Section/Section'
 import reactQuizImage from '../../assets/images/react-quiz.webp'
 import spreadsheetsImage from '../../assets/images/spreadsheets.webp'
 import reactHooksImage from '../../assets/images/react-hooks.webp'
 import centerLogisticImage from '../../assets/images/center-logistic.webp'
 import mafiaRandomizer from '../../assets/images/mafia-randomizer.webp'
-import {Tag} from '../UI/Tag/Tag'
+import feedbackReceiver from '../../assets/images/feedback-receiver-client.webp'
+import angularCurrencyConverter from '../../assets/images/angular-currency-convert.webp'
+import { Tag } from '../UI/Tag/Tag'
 import classes from './Projects.module.scss'
 
 const tiltEffectSettings = {
@@ -14,42 +16,80 @@ const tiltEffectSettings = {
   perspective: 800, // transform perspective, the lower the more extreme the tilt gets (pixels (px))
   scale: 1, // transform scale - 2 = 200%
   speed: 500, // speed (transition-duration) of the enter/exit transition (milliseconds (ms))
-  easing: 'cubic-bezier(0, 1, 1, 1)' // easing (transition-timing-function) of the enter/exit transition
+  easing: 'cubic-bezier(0, 1, 1, 1)', // easing (transition-timing-function) of the enter/exit transition
 }
 
 const zoomEffectSettings = {
   size: '200%',
   speed: 500, // speed (transition-duration) of the enter/exit transition (milliseconds (ms))
-  easing: 'cubic-bezier(0, 1, 1, 1)' // easing (transition-timing-function) of the enter/exit transition
+  easing: 'cubic-bezier(0, 1, 1, 1)', // easing (transition-timing-function) of the enter/exit transition
 }
 
 const projectsList = [
-	{title: 'React Quiz', image: reactQuizImage, linkLive: 'https://react-quiz-382ea.web.app', linkGithub: 'https://github.com/Shamanovi4/react-quiz',
-		description: `React based SPA which allows you to create and store custom
-    quizzes with register and login functionality. Initial idea was to
-    create a Google Forms clone but with less functionality. Written
-    with the use of HTML, SCSS, JS, React, React Redux, React Router,
-    Axios. Uses Firebase Realtime Databse to store quizzes.`},
-	{title: 'Spreadsheets', image: spreadsheetsImage, linkLive: 'https://spreadsheets-7be4e.web.app', linkGithub: 'https://github.com/Shamanovi4/spreadsheets',
-		description: `Based of a JavaScript Course this project was done and enhanced 
+  {
+    title: 'React Quiz',
+    image: reactQuizImage,
+    linkLive: 'https://react-quiz-382ea.web.app',
+    linkGithub: 'https://github.com/Shamanovi4/react-quiz',
+    description: `React based SPA which allows you to create and store custom quizzes 
+    with register and login functionality. Initial idea was to create a Google Forms 
+    clone but with less functionality. Written with the use of React, React Redux, 
+    React Router, Axios. Uses Firebase Realtime Databse to store quizzes.`,
+  },
+  {
+    title: 'Spreadsheets',
+    image: spreadsheetsImage,
+    linkLive: 'https://spreadsheets-7be4e.web.app',
+    linkGithub: 'https://github.com/Shamanovi4/spreadsheets',
+    description: `Based of a JavaScript Course this project was done and enhanced
     by me to develop and consolidate my vanilla JS skills. The goal of
     this project was to create vanilla JS framework from scratch and
-    then create some kind of Google Spreadsheets clone. This SPA was
-    written with the use of HTML, SCSS, JS, Redux (written from
-    scratch), BEM, Webpack.`},
-  {title: 'Mafia Randomizer', image: mafiaRandomizer, linkLive: 'https://mafia-randomizer.web.app/', linkGithub: 'https://github.com/Shamanovi4/mafia-randomizer',
-		description: `Roles randomizer, timer and other tools for Mafia intelligence game. Written with React and Typescript.`},
-  {title: 'React Hooks', image: reactHooksImage, linkLive: 'https://react-hooks-a59de.web.app', linkGithub: 'https://github.com/Shamanovi4/react-hooks',
-		description: `Simple React app for searching and fetching data from github, e.g.,
-    profiles, repositories and other. Written with modern React Hooks
-    with the use of HTML, SCSS, JS, React, React Redux, React Router,
-    Axios.`},
-	{title: 'Center Logistic', image: centerLogisticImage, linkLive: 'https://center-logistic.web.app', linkGithub: 'https://github.com/Shamanovi4/center-logistic',
-		description: `Transport company website written with the use of Pug, BEM, SCSS, JQuery, Gulp. Website is fully responsive`},
+    then create some kind of Google Spreadsheets clone.`,
+  },
+  {
+    title: 'Mafia Randomizer',
+    image: mafiaRandomizer,
+    linkLive: 'https://mafia-randomizer.web.app/',
+    linkGithub: 'https://github.com/Shamanovi4/mafia-randomizer',
+    description: `Typescript React based SPA for mafia game that includes roles
+    randomization, timer and tools for creating, storing and editing
+    game data.`,
+  },
+  {
+    title: 'React Hooks',
+    image: reactHooksImage,
+    linkLive: 'https://react-hooks-a59de.web.app',
+    linkGithub: 'https://github.com/Shamanovi4/react-hooks',
+    description: `Simple React app for searching and fetching data from github, e.g.,
+    profiles, repositories and other. Written with React, React Redux, React Router,
+    Axios.`,
+  },
+  {
+    title: 'Center Logistic',
+    image: centerLogisticImage,
+    linkLive: 'https://center-logistic.web.app',
+    linkGithub: 'https://github.com/Shamanovi4/center-logistic',
+    description: `Transport company website written with the use of Pug, BEM, SCSS, JQuery, Gulp. Website is fully responsive`,
+  },
+  {
+    title: 'Feedback Receiver',
+    image: feedbackReceiver,
+    linkLive: 'https://feedback-receiver-client.onrender.com/',
+    linkGithub: 'https://github.com/Shamanovi4/feedback-receiver-client',
+    description: `NestJS back end part and React front end app that sends
+    feedback messages to PostgreSQL database.`,
+  },
+  {
+    title: 'Currency Converter',
+    image: angularCurrencyConverter,
+    linkLive: 'https://angular-currency-convert-7dead.web.app/',
+    linkGithub: 'https://github.com/Shamanovi4/angular-currency-converter',
+    description: `Currency conversion app written with Angular 14. App gets currency values via api and allows to convert them or swap places.`,
+  },
 ]
 
 export const Projects = () => {
-	function cardSetTransition(event) {
+  function cardSetTransition(event) {
     const card = event.currentTarget
 
     clearTimeout(card.transitionTimeoutId)
@@ -58,36 +98,49 @@ export const Projects = () => {
     card.transitionTimeoutId = setTimeout(() => {
       card.style.transition = ''
     }, tiltEffectSettings.speed)
-	}
+  }
 
-	function cardMouseMove(event) {
-		if (typeof(event.target.className) === 'string') {
-			if (!event.target.className.includes('Button') && !event.target.className.includes('Icon')) {
-				const card = event.currentTarget
-				const cardWidth = card.offsetWidth
-				const cardHeight = card.offsetHeight
-				const centerX = cardWidth / 2
-				const centerY = cardHeight / 2
-				const mouseX = event.nativeEvent.offsetX - centerX
-				const mouseY = event.nativeEvent.offsetY - centerY
-				const rotateXUncapped = (+1) * tiltEffectSettings.max * mouseY / (cardHeight / 2)
-				const rotateYUncapped = (-1) * tiltEffectSettings.max * mouseX / (cardWidth / 2)
-				const rotateX = rotateXUncapped < -tiltEffectSettings.max ? -tiltEffectSettings.max : 
-						(rotateXUncapped > tiltEffectSettings.max ? tiltEffectSettings.max : rotateXUncapped)
-				const rotateY = rotateYUncapped < -tiltEffectSettings.max ? -tiltEffectSettings.max : 
-						(rotateYUncapped > tiltEffectSettings.max ? tiltEffectSettings.max : rotateYUncapped)
-			
-				card.style.transform = `perspective(${tiltEffectSettings.perspective}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) 
+  function cardMouseMove(event) {
+    if (typeof event.target.className === 'string') {
+      if (
+        !event.target.className.includes('Button') &&
+        !event.target.className.includes('Icon')
+      ) {
+        const card = event.currentTarget
+        const cardWidth = card.offsetWidth
+        const cardHeight = card.offsetHeight
+        const centerX = cardWidth / 2
+        const centerY = cardHeight / 2
+        const mouseX = event.nativeEvent.offsetX - centerX
+        const mouseY = event.nativeEvent.offsetY - centerY
+        const rotateXUncapped =
+          (+1 * tiltEffectSettings.max * mouseY) / (cardHeight / 2)
+        const rotateYUncapped =
+          (-1 * tiltEffectSettings.max * mouseX) / (cardWidth / 2)
+        const rotateX =
+          rotateXUncapped < -tiltEffectSettings.max
+            ? -tiltEffectSettings.max
+            : rotateXUncapped > tiltEffectSettings.max
+            ? tiltEffectSettings.max
+            : rotateXUncapped
+        const rotateY =
+          rotateYUncapped < -tiltEffectSettings.max
+            ? -tiltEffectSettings.max
+            : rotateYUncapped > tiltEffectSettings.max
+            ? tiltEffectSettings.max
+            : rotateYUncapped
+
+        card.style.transform = `perspective(${tiltEffectSettings.perspective}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) 
 						scale3d(${tiltEffectSettings.scale}, ${tiltEffectSettings.scale}, ${tiltEffectSettings.scale})`
-			}
-		}
-	}
+      }
+    }
+  }
 
-	function cardMouseLeave(event) {
-		event.currentTarget.style.transform = `perspective(${tiltEffectSettings.perspective}px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)`
-		
+  function cardMouseLeave(event) {
+    event.currentTarget.style.transform = `perspective(${tiltEffectSettings.perspective}px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)`
+
     cardSetTransition(event)
-	}
+  }
 
   function imageSetTransition(event) {
     const image = event.currentTarget
@@ -97,17 +150,17 @@ export const Projects = () => {
     image.style.transition = `all ${zoomEffectSettings.speed}ms ${zoomEffectSettings.easing}`
     image.transitionTimeoutId = setTimeout(() => {
       image.style.transition = ''
-		}, zoomEffectSettings.speed)
-	}
+    }, zoomEffectSettings.speed)
+  }
 
   function imageMouseMove(event) {
     const image = event.currentTarget
     const imageWidth = image.offsetWidth
     const imageHeight = image.offsetHeight
     const mouseX = event.nativeEvent.offsetX
-		const mouseY = event.nativeEvent.offsetY 
-    const backgroundPositionX = mouseX / imageWidth * 100
-    const backgroundPositionY = mouseY / imageHeight * 100
+    const mouseY = event.nativeEvent.offsetY
+    const backgroundPositionX = (mouseX / imageWidth) * 100
+    const backgroundPositionY = (mouseY / imageHeight) * 100
 
     image.style.backgroundPosition = `${backgroundPositionX}% ${backgroundPositionY}%`
     image.style.backgroundSize = zoomEffectSettings.size
@@ -121,48 +174,63 @@ export const Projects = () => {
     imageSetTransition(event)
   }
 
-	function renderProjectsList(list) {
-		return list.map((item, index) => {
-			return (
-				<li className={classes.projectsListCard}
-					key={index}
-					onMouseEnter={event => cardSetTransition(event)}
-					onMouseMove={event => cardMouseMove(event)}
-					onMouseLeave={event => cardMouseLeave(event)}
-				>
-					<div className={classes.projectsListItem}>
-						<div className={classes.projectsListItemHeading}>
-							<h2 className={classes.projectsListItemTitle}>{item.title}</h2>
-              <a className={classes.projectsListItemIcon} href={item.linkGithub} target='_blank' rel='noreferrer'>
+  function renderProjectsList(list) {
+    return list.map((item, index) => {
+      return (
+        <li
+          className={classes.projectsListCard}
+          key={index}
+          onMouseEnter={(event) => cardSetTransition(event)}
+          onMouseMove={(event) => cardMouseMove(event)}
+          onMouseLeave={(event) => cardMouseLeave(event)}
+        >
+          <div className={classes.projectsListItem}>
+            <div className={classes.projectsListItemHeading}>
+              <h2 className={classes.projectsListItemTitle}>{item.title}</h2>
+              <a
+                className={classes.projectsListItemIcon}
+                href={item.linkGithub}
+                target='_blank'
+                rel='noreferrer'
+              >
                 <FaGithub className={classes.projectsListItemIconSvg} />
               </a>
-						</div>
-						<div className={classes.projectsListItemImageWrapper}>
-							<div className={classes.projectsListItemImage} alt=''
-                style={{backgroundImage: `url(${item.image})`}}
-                onMouseEnter={event => imageSetTransition(event)}
-                onMouseMove={event => imageMouseMove(event)}
-					      onMouseLeave={event => imageMouseLeave(event)}
+            </div>
+            <div className={classes.projectsListItemImageWrapper}>
+              <div
+                className={classes.projectsListItemImage}
+                alt=''
+                style={{ backgroundImage: `url(${item.image})` }}
+                onMouseEnter={(event) => imageSetTransition(event)}
+                onMouseMove={(event) => imageMouseMove(event)}
+                onMouseLeave={(event) => imageMouseLeave(event)}
               />
-						</div>
-						<p className={classes.projectsListItemDescription}>{item.description}</p>
-						<a className={classes.projectsListItemButton} href={item.linkLive} target='_blank' rel='noreferrer'>
+            </div>
+            <p className={classes.projectsListItemDescription}>
+              {item.description}
+            </p>
+            <a
+              className={classes.projectsListItemButton}
+              href={item.linkLive}
+              target='_blank'
+              rel='noreferrer'
+            >
               Open live version
             </a>
-					</div>
-				</li>
-			)
-		})
-	}
+          </div>
+        </li>
+      )
+    })
+  }
 
-	return (
-		<Section>
-			<div className={classes.projects}>
-				<Tag>Projects</Tag>
-			</div>
-			<ul className={classes.projectsList}>
-				{renderProjectsList(projectsList)}
-			</ul>
-		</Section>
-	)
+  return (
+    <Section>
+      <div className={classes.projects}>
+        <Tag>Projects</Tag>
+      </div>
+      <ul className={classes.projectsList}>
+        {renderProjectsList(projectsList)}
+      </ul>
+    </Section>
+  )
 }
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,51 +1,108 @@
 import React from 'react'
-import {Section} from '../../hoc/Section/Section'
-import {FaHtml5, FaCss3, FaReact, FaGit, FaNpm, FaGulp, FaFigma} from 'react-icons/fa'
-import {SiJavascript, SiWebpack, SiAdobephotoshop, SiTypescript} from 'react-icons/si'
+import { Section } from '../../hoc/Section/Section'
+import {
+  FaHtml5,
+  FaCss3,
+  FaReact,
+  FaGit,
+  FaGulp,
+  FaFigma,
+  FaAngular,
+  FaNodeJs,
+} from 'react-icons/fa'
+import {
+  SiJavascript,
+  SiWebpack,
+  SiAdobephotoshop,
+  SiTypescript,
+  SiNestjs,
+  SiPostgresql,
+} from 'react-icons/si'
 import classes from './Skills.module.scss'
-import {Tag} from '../UI/Tag/Tag'
+import { Tag } from '../UI/Tag/Tag'
 
 export const Skills = () => {
-	const primarySkillsList = [
-		{icon: <FaHtml5 />, name: 'HTML', details: 'HTML5, Pug, BEM Methodology', color: '#e34f26'},
-		{icon: <FaCss3 />, name: 'CSS', details: 'CSS3, Responsive, SASS, SCSS', color: '#2965f1'},
-		{icon: <SiJavascript />, name: 'JavaScript', details: 'ES6, Redux, Axios, JQuery', color: '#f7df1e'},
-		{icon: <FaReact />, name: 'React', details: 'Hooks, Router, Redux', color: '#61dafb'},
-    {icon: <SiTypescript />, name: 'Typescript', details: '', color: '#3178c6'},
-	]
-	const secondarySkillsList = [
-		{icon: <FaGit />, name: 'Git', color: '#f54d27'},
-		{icon: <FaNpm />, name: 'Npm', color: '#cb3837'},
-		{icon: <FaGulp />, name: 'Gulp', color: '#cf4647'},
-		{icon: <SiWebpack />, name: 'Webpack', color: '#1c78c0'},
-		{icon: <FaFigma />, name: 'Figma', color: '#c7b9ff'},
-		{icon: <SiAdobephotoshop />, name: 'Photoshop', color: '#31a8ff'},
-	]
+  const primarySkillsList = [
+    {
+      icon: <FaHtml5 />,
+      name: 'HTML',
+      details: 'HTML5, Pug, BEM',
+      color: '#e34f26',
+    },
+    {
+      icon: <FaCss3 />,
+      name: 'CSS',
+      details: 'CSS3, Responsive, SASS, SCSS',
+      color: '#2965f1',
+    },
+    {
+      icon: <SiJavascript />,
+      name: 'JavaScript',
+      details: 'ES9, Redux, Axios, JQuery',
+      color: '#f7df1e',
+    },
+    {
+      icon: <FaReact />,
+      name: 'React',
+      details: 'Hooks, Router, Redux',
+      color: '#61dafb',
+    },
+    {
+      icon: <SiTypescript />,
+      name: 'Typescript',
+      details: '',
+      color: '#3178c6',
+    },
+    { icon: <FaAngular />, name: 'Angular 14', details: '', color: '#dd0031' },
+    { icon: <FaNodeJs />, name: 'Node.js', details: '', color: '#57924f' },
+    { icon: <SiNestjs />, name: 'NestJS', details: '', color: '#e0234e' },
+    {
+      icon: <SiPostgresql />,
+      name: 'PostgreSQL',
+      details: '',
+      color: '#336791',
+    },
+  ]
+  const secondarySkillsList = [
+    { icon: <FaGit />, name: 'Git', color: '#f54d27' },
+    { icon: <FaGulp />, name: 'Gulp', color: '#cf4647' },
+    { icon: <SiWebpack />, name: 'Webpack', color: '#1c78c0' },
+    { icon: <FaFigma />, name: 'Figma', color: '#c7b9ff' },
+    { icon: <SiAdobephotoshop />, name: 'Photoshop', color: '#31a8ff' },
+  ]
 
-	function renderSkillsList(list) {
-		return list.map((item, index) => {
-			return (
-				<li className={classes.skillsListItem} key={index}>
-					<i className={classes.skillsListItemIcon} style={{color: item.color}}>{item.icon}</i>
-					<h2 className={classes.skillsListItemName}>{item.name}</h2>
-					<p className={classes.skillsListItemDetails}>{item.details}</p>
-				</li>
-			)
-		})
-	}
+  function renderSkillsList(list) {
+    return list.map((item, index) => {
+      return (
+        <li
+          className={classes.skillsListItem}
+          key={index}
+        >
+          <i
+            className={classes.skillsListItemIcon}
+            style={{ color: item.color }}
+          >
+            {item.icon}
+          </i>
+          <h2 className={classes.skillsListItemName}>{item.name}</h2>
+          <p className={classes.skillsListItemDetails}>{item.details}</p>
+        </li>
+      )
+    })
+  }
 
-	return (
-		<Section>
-			<div className={classes.skills}>
-				<Tag>PrimarySkills</Tag>
-				<ul className={classes.skillsList}>
-					{renderSkillsList(primarySkillsList)}
-				</ul>
-				<Tag>SecondarySkills</Tag>
-				<ul className={classes.skillsList}>
-					{renderSkillsList(secondarySkillsList)}
-				</ul>
-			</div>
-		</Section>
-	)
+  return (
+    <Section>
+      <div className={classes.skills}>
+        <Tag>PrimarySkills</Tag>
+        <ul className={classes.skillsList}>
+          {renderSkillsList(primarySkillsList)}
+        </ul>
+        <Tag>SecondarySkills</Tag>
+        <ul className={classes.skillsList}>
+          {renderSkillsList(secondarySkillsList)}
+        </ul>
+      </div>
+    </Section>
+  )
 }
